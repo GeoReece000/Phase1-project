d0163db5b625c0604a1ed5d74d9c19200ddb322c
GeoReece000 <geofreytegeret@gmail.com>
2023-04-06 21:41:43 +0300

    reference HTML elements

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const endpoint = 'https://api.exchangerate.host/latest?base=USD';
 const convertURL = 'https://api.exchangerate.host/convert';
 const timeseriesURL = 'https://api.exchangerate.host/timeseries';
+
+// Get references to HTML elements
+const form = document.querySelector('form');
+const dateInput = document.getElementById('date-input');
+const ratesDisplay = document.getElementById('rates-display');
