5e7d01fc8718ddc4cf186fdd71fff048305c7086
GeoReece000 <geofreytegeret@gmail.com>
2023-04-06 21:39:27 +0300

    Define api endpoints

---
+++ index.js
@@ -0,0 +1,4 @@
+// Define API endpoints
+const endpoint = 'https://api.exchangerate.host/latest?base=USD';
+const convertURL = 'https://api.exchangerate.host/convert';
+const timeseriesURL = 'https://api.exchangerate.host/timeseries';
