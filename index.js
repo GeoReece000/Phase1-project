4d65f5103b19d10eac241daf94d7728c9e1263fc
GeoReece000 <geofreytegeret@gmail.com>
2023-04-06 21:42:48 +0300

    loop through exchange rates

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ const timeseriesURL = 'https://api.exchangerate.host/timeseries';
 const form = document.querySelector('form');
 const dateInput = document.getElementById('date-input');
 const ratesDisplay = document.getElementById('rates-display');
+
+// Fetch latest exchange rates and display them in a dropdown menu
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        // Get a reference to the currency dropdown menu
+        const currencySelect = document.querySelector('#currency');
+        // Loop through the exchange rates and add them as options to the dropdown
+        for (const [currency, rate] of Object.entries(data.rates)) {
+            const option = document.createElement('option');
+            option.value = currency;
+            option.text = currency;
+            currencySelect.appendChild(option);
+        }
+        // Update the exchange rate display when the user selects a currency
+        currencySelect.addEventListener('change', () => {
+            const selectedCurrency = currencySelect.value;
+            const selectedRate = data.rates[selectedCurrency];
+            const rateDiv = document.querySelector('#rate');
+            rateDiv.textContent = `1 USD = ${selectedRate.toFixed(4)} ${selectedCurrency}`;
+        });
+    })
+    .catch(error => console.error(error));
